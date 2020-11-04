41e12bca0f35a728c0e1d41f0dde5b75b90913f3
Ted Chen <teddy70642000@gmail.com>
2020-11-04 17:57:53 +0800

    coupon model create

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_035321) do
+ActiveRecord::Schema.define(version: 2020_11_04_095500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "uuid"
+    t.string "name"
+    t.string "twid"
+    t.string "phone"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_coupons_on_user_id", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +38,5 @@ ActiveRecord::Schema.define(version: 2020_11_04_035321) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "coupons", "users"
 end
