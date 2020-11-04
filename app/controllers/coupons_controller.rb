caf929b95e9094297c2fac1f54888a687cb3ecb5
Ted Chen <teddy70642000@gmail.com>
2020-11-04 21:59:13 +0800

    coupon controller - add before_action check whether user
    Rspec
    CouponsController
    - user setting
    - default setting, user sign_in

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,4 +1,7 @@
 class CouponsController < ApplicationController
+  
+  before_action :authenticate_user!
+  
   def new
     @coupon = Coupon.new
   end
--- spec/controller/coupon_controller_spec.rb
+++ spec/controller/coupon_controller_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CouponsController, type: :controller do
+  let(:user) do
+    User.create(
+      email: Faker::Internet.email,
+      password: Faker::String.random
+    )
+  end
 
+  # before_action 
+  before do
+    sign_in user
+  end
+  
 end
