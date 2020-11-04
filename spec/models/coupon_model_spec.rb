0f74ee5abf1d3261060e3d09a120215e8805eb2b
Ted Chen <teddy70642000@gmail.com>
2020-11-04 21:41:37 +0800

    Rspec Coupon model - create   - whether coupon can be create   - get coupon data

--- spec/models/coupon_model_spec.rb
+++ spec/models/coupon_model_spec.rb
@@ -66,6 +66,29 @@ RSpec.describe Coupon, type: :model do
     end
   end
 
-  
+  describe '.create' do 
+    context 'with name, phone, twid(faker)' do
+      it 'should be ok' do
+        coupon = user.create_coupon(
+          name: Faker::Name.name  ,
+          phone: Faker::PhoneNumber.cell_phone,
+          twid: TwidGenerator.generate
+        )
+        expect(coupon).to eq(Coupon.last)
+      end
+    end
+
+    context 'with name, phone, twid' do
+      it 'should be ok' do
+        coupon = user.create_coupon(
+          name: 'Apple'  ,
+          phone: '0911059123',
+          twid: TwidGenerator.generate
+        )
+        expect(coupon.name).to eq('Apple')
+        expect(coupon.phone).to eq('0911059123')
+      end
+    end
+  end
 
 end
