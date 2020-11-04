22e8733f1883a7586aec1454438929b2388599dc
Ted Chen <teddy70642000@gmail.com>
2020-11-04 18:10:08 +0800

    coupon - controller   - clean params, merge(user: current_user)   - #create     - ok, redirect_to root and show notice(with uuid)     - fail, render new
    User
    - model
      - before_create uuid
      - validates
        - name, must be exist, customize error message
        - phone, must be exist, customize error message

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,6 +4,20 @@ class CouponsController < ApplicationController
   end
 
   def create
+    @coupon = Coupon.new(coupon_params)
+    
+    if @coupon.save
+      redirect_to root_path, notice: "已成功兌換，您的兌換卷序號為 #{ @coupon.uuid }"
+    else
+      render :new
+    end
 
+  end
+
+  private
+  def coupon_params
+    params.require(:coupon)
+          .permit(:name, :twid, :phone)
+          .merge(user: current_user)
   end
 end
--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -1,3 +1,17 @@
 class Coupon < ApplicationRecord
   belongs_to :user
+
+  validates :name , presence: {message: '姓名不可為空' }
+  validates :phone, presence: {message: '手機號碼不可為空' }
+
+  before_create :create_uuid
+ 
+  private
+  def create_uuid
+    self.uuid = serial_generator(10)
+  end
+
+  def serial_generator(n)
+    [*'a'..'z', *'A'..'Z', *0..9].sample(n).join
+  end
 end
